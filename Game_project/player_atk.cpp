bab591489153229936789d668012b0f73107344b
Min826 <129669485+Min826@users.noreply.github.com>
2023-04-16 22:15:03 +0800

    Add files via upload

--- Game_project/player_atk.cpp
+++ Game_project/player_atk.cpp
@@ -7,33 +7,24 @@ extern double start, end;
 extern Human player;
 extern Monster enemy[2];
 
+//玩家攻擊敵人
+void atk_enemy(Monster &e, int power, int damage, char dir){
+	if(is_middle(e.x + e.width * 0.98, player.x + player.width, e.x + e.width * 0.02))
+	{
+		e.hp -= damage;
+		e.x += (dir == 'd' ? power : -power);// 敵人會被玩家擊退 
+		player.atked = 1;// 若有造成傷害則會記錄 避免一次攻擊有多次傷害 
+	}
+}
+
 // 玩家攻擊 
 void player_atk(int val)
 {	
 	switch(enemy_num)
 	{
 		case 0:
-			if(player.dir == 'd')
-			{	
-				// 若敵人在玩家攻擊範圍 
-				if(is_middle(enemy[0].x + enemy[0].width * 0.98, player.x + player.width, enemy[0].x + enemy[0].width * 0.02))
-				{
-					enemy[0].hp -= player.damage;
-					enemy[0].x += player.power;// 敵人會被玩家擊退 
-					player.atked = 1;// 若有造成傷害則會記錄 避免一次攻擊有多次傷害 
-				}
-			}
-			else
-			{
-				if(is_middle(enemy[0].x + enemy[0].width * 0.98, player.x, enemy[0].x + enemy[0].width * 0.02))
-				{
-					enemy[0].hp -= player.damage;
-					enemy[0].x -= player.power;	
-					player.atked = 1;
-				}
-			}
+			atk_enemy(enemy[0], player.power, player.damage, player.dir);
 			break;
-		
 		default :
 			break;
 	}
